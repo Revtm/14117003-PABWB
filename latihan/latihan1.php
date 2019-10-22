229364b5c03856d29d8f9f36267572d383179433
revtm <kykylizaro98@gmail.com>
2019-10-22 14:36:13 +0700

    tambah minggu-7

---
+++ latihan/latihan1.php
@@ -0,0 +1,20 @@
+<?php
+  function tukar(&$a, &$b){
+    $temp = $a;
+    $a = $b;
+    $b = $temp;
+  }
+
+  $x = 5;
+  $y = 7;
+
+  echo "x : $x </br>";
+  echo "y : $y </br>";
+
+  tukar($x, $y);
+
+  echo "</br> setelah tukar </br>";
+  echo "x : $x </br>";
+  echo "y : $y </br>";
+
+ ?>
